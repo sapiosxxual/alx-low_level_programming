a9e27c9a3e36193a5d9395fd1ffa17eee5fec1f4
root <okonjivictor49@gmail.com>
2023-08-10 14:33:41 -0700

    using puts to print output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - using puts to print print output
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
