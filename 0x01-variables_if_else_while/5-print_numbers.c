d95ae26f089577cd49b0175f6072934b6f8fa644
root <okonjivictor49@gmail.com>
2023-08-11 21:06:30 -0700

    it print all single digits of base ten inc zero

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -print all single digit in base ten inc zero
+ *
+ * Return: always(0)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{	putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
