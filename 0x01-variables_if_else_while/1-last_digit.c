fd2a7fc9b9ed64f95ee822dd9d636c735f4fb64b
root <okonjivictor49@gmail.com>
2023-08-11 19:19:56 -0700

    assigning a random number then comparing the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ int lastd;
 	lastd = n % 10;
 	if (lastd > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5/n", n, lastd);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	{
-		printf("Last digit of %d is %d and is 0/n", n, lastd);
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else if (lastd < 6 && lastd != 0)
 	{
