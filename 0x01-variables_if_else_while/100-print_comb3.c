2cf9d6782dcfbec7585ea40787da922a93f495dd
root <okonjivictor49@gmail.com>
2023-08-11 21:59:56 -0700

    ej

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - orijt
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+	
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -print hexadecimal'
+ *
+ * Return: always (0);
+ */
+int main(void)
+{
+	char numbers = '0';
+	char letters = 'a';
+
+	while (numbers < '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	while (letters <= 'f')
+	{
+		putchar(letters);
+		letters++;
+	}
+	putchar('\n');
+	return (0);
+}
