a543d9fe0fec1a51483b942176f0e02cdf2db3eb
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-17 09:59:05 -0700

    checks for a digit through 0 to 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,11 @@
  * _isdigit - checks for a digit (0 through 9)
  *@c: digit entered
  *
- * Return: (1) if true (0) if false
+ * Return: (1) if its a digit otherwise (0)
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9';)
 	{
 		return (1);
 	}
