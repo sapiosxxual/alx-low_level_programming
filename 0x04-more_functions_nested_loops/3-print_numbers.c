e0dc1b467be9df1a784cc56ce4873c767a5ad788
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-17 11:03:30 -0700

    prints the numbers, from 0 to 9, followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
  * print_numbers - prints the numbers, from 0 to 9
- * @i: starting point
- * Description: using _putchar to print output;
+ *
  * Return: no return
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		_putchar(i);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
