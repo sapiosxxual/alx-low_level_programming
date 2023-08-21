734cb3a0288c6bac6722089d1f303eed236f591b
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-21 16:31:46 -0700

    prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,12 @@
 /**
  *_puts-print a string fllwd nl
  *Description: using _putchar to disply output
- *@i: first c in string
+ *@str: string to be printed
  */
 void _puts(char *str)
+
 {
+	char *str;
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
