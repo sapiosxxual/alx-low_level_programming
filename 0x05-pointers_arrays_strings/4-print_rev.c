2565eb7f1c7f61740091c775bcafd281c5c1cab5
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-21 17:29:38 -0700

    prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev-prints a string, in reverse, followed by a new line
+ *@s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i];)
+	{
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
