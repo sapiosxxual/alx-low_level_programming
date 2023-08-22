e1e17559375013c2b6fa57668e824458a00417a3
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-21 21:52:02 -0700

    return dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcpy- pointer to a charcater
+ *
+ * @dest: string 1 pointer to a char
+ * @src: string 2 pointer to a char
+ *
+ * Return: character(dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
