72bda2f1c04d5b754183c8ce5728d27c44503980
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-22 02:20:15 -0700

    generate random valid passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	srand(time(NULL));
 
-	char valid_chars[ ] = "abcdefghijklmopqrstuvwxyzABCEFINOPQRSVWXYZ0123456789";
+	int i;
+	char valid_chars[] = "abcdefghijklmopqrstuvwxyzABCEFINOPQRSVWXYZ0123456789";
 	int password_length = 11;
 	char password[12];
 
 	for (int i = 0; i < password_length; i++)
 	{
-		int random_index = rand() % (sizeof(valid_chars) -1);
+		int random_index = rand() % (sizeof(valid_chars) - 1);
 
 		password[i] = valid_chars[random_index];
 	}
