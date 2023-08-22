ad5f0e92acef5ed3ef8486126b536eeb5a4dbde2
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-21 19:55:48 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,35 @@
 #include "main.h"
 /**
+ * _strlen-string length
+ * @s: the string
+ * Description: using _strlen to get length of string
+ * Return: length of character
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0';)
+	{
+		n++;
+	}
+	return (n);
+}
+/**
  * rev_string-reverses a string
  * @s: string to be reversed
- * Description: using _strlen to get the length of character
- */
+*/
 void rev_string(char *s)
 {
-	int length = _strlen(s);
-	char temp;
 	int i = 0;
-	int end = length - i - 1;
+	int l = _strlen(s);
+	char temp;
+	int end = l - i - 1;
 
 	while (i < end)
 	{
 		temp = s[i];
-		s[i] = s[length - i - 1];
+		s[i] = s[l - i - 1];
 		s[end] = temp;
 
 		i++;
