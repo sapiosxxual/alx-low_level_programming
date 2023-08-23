9d7bea026063dd0aeef4c8fc22a7b9f32706ae5e
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-23 15:51:16 -0700

    reverses array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,8 @@ void reverse_array(int *a, int n)
 	while (i < n / 2)
 	{
 		temp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = temp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 		i++;
 	}
 }
