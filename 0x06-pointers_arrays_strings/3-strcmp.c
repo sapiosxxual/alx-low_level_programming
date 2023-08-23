f93fd4e393708e26048b916a15a3763108cf91f9
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-23 15:26:29 -0700

    reverses array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,6 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
 	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev-array-reverses the array content
+ * @a: array
+ * @n: number of array content
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+		i++;
+	}
+}
