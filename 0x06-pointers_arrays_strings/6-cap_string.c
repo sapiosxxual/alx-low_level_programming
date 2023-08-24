d94572fa0a902ed716d0742921fe03163154326d
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-23 18:45:19 -0700

    capitalize first char of word and sfter symbol

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string- capitalizes first char of each word in string
+ * @str: string
+ * Descriptio: using string_toupper to change lower to upper
+ * Return: str after function
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char c[] = {44, 59, 46, 33, 63, '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+			i++;
+		}
+	while (c[j] != '\0')
+	{
+		if (c[j] == str[i] && str[i + i] >= 'a' && str[i + 1] <= 'z')
+		{
+			str[i + i] = str[i + 1] - 32;
+			j++;
+		}
+	}
+	}
+	return (str);
+}
