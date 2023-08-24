7b7b3864d72b086837f49100aa119a963bd1cbc6
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-23 17:34:33 -0700

    lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper-changes all lowercase letters of a string to uppercase
+ * @str: string
+ * Return: string after changing lower to upper
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		char lower = 'a';
+		char upper = 'A';
+
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - upper + lower;
+		}
+		i++;
+	}
+
+	return (str);
+}
