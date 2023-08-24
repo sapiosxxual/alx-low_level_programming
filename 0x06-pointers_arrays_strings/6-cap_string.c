6b71881d2ee7ae480f29ecdb75cca6837a272a06
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-23 19:15:24 -0700

    capitalize first char of word and sfter symbol

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,25 @@
 #include "main.h"
 /**
- * cap_string- capitalizes first char of each word in string
+ * cap_string-capitalizes first char of each word in string
  * @str: string
  * Return: str after function
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int j = 0;
-	char c[] = {44, 59, 46, 33, 63, '"', '(', ')', '{', '}', ' ', '\t', '\n'};
+	int i, j;
+	char c[] = {44, 59, 46, '.', 63, '"', '(', ')', '{', '}', ' ', '\t', '\n'};
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
-			i++;
 		}
-	while (c[j] != '\0')
+	for (j = 0; c[j] != '\0'; i++)
 	{
-		if (c[j] == str[i] && str[i + i] >= 'a' && str[i + 1] <= 'z')
+		if (c[j] == str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 		{
-			str[i + i] = str[i + 1] - 32;
-			j++;
+			str[i + 1] = str[i + 1] - 32;
 		}
 	}
 	}
