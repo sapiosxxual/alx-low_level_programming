73626447516a6314c7736824fd19d0944a4bbb1a
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-08-23 19:50:50 -0700

    encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * leet - Encodes a string into "1337" text.
  * @str: The input string.
@@ -7,19 +6,18 @@
  */
 char *leet(char *str)
 {
-	char leet_map[256] = {0};
-
-	leet_map['a'] = leet_map['A'] = '4';
-	leet_map['e'] = leet_map['E'] = '3';
-	leet_map['o'] = leet_map['O'] = '0';
-	leet_map['t'] = leet_map['T'] = '7';
-	leet_map['l'] = leet_map['L'] = '1';
+	char *leet_chars = "aeotlAEOTL";
+	char *leet_subs = "43071031";
 
 	for (int i = 0; str[i] != '\0'; i++)
 	{
-		if (leet_map[str[i]] != 0)
+		for (int j = 0; leet_chars[j] != '\0'; j++)
 		{
-			str[i] = leet_map[str[i]];
+			if (str[i] == leet_chars[j])
+			{
+				str[i] = leet_subs[j];
+				break;
+			}
 		}
 	}
 
