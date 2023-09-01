e5c455344a355d029d4b510a8cf61cc082cfc025
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-01 13:43:50 -0700

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,8 +1,10 @@
-#include "unistd.h"
+#include "main.h"
+#include <unistd.h>
 /**
- * _putchar-print charcater(s) as output
- * Return: On Success 1.
- * On error, -1 is returned and error is set approrpraitely
+ * _putchar-display character
+ * @c: charcater to be printed
+ * Return: On Success 1;
+ * On error
  */
 int _putchar(char c)
 {
--- 0x09-static_libraries/main.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-int main(void)
-{
-	_puts("\"AnikeJewels\"\n\t- Bunmi");
-
-	return (0);
-}
