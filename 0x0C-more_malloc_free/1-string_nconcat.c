37845127dc5be65a247fdc4d07289931bd5a313b
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-07 14:14:44 -0700

    function that concatenates two strings using n bytes of second string, if n >= 2nd string len use 2nd string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,15 +6,12 @@
  * @s1: first string
  * @s2: second string
  * @n: n bytes of s2
- * Return: the nee string ater concatenating
+ * Return: the new string ater concatenating
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t len1 = 0;
-	size_t len2 = 0;
-	size_t total_len = 0;
+	size_t len1 = 0, len2 = 0, total_len = 0, i = 0, j = 0;
 	char *newString;
-	size_t i, j = 0;
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -23,33 +20,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 		len2 = 0;
 	}
-	else
-	{
-		while (s2[len2] != '\0')
+	while (s2[len2] != '\0')
 		len2++;
-	}
 	if (len2 > n)
 		total_len = len1 + n + 1;
 	if (len2 <= n)
 		total_len = len1 + len2 + 1;
-
 	newString = malloc(sizeof(char) * total_len);
 	if (newString == NULL)
 		return (NULL);
 	for (i = 0, j = 0; i < total_len - 1; i++)
 	{
 		if (i < len1)
-		{
 			newString[i] = s1[i];
-		}
 		else
 		{
 			newString[i] = s2[j];
 			j++;
 		}
 	}
-
 	newString[i] = '\0';
 	return (newString);
 }
-
