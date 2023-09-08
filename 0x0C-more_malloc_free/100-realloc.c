da7933eaa30e2b4b72ad70fb825067daef9a6303
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-07 20:44:52 -0700

    reallocates a memory block using malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		memcpy(new_ptr, ptr, old_size);
+	}
+	else
+	{
+		memcpy(new_ptr, ptr, new_size);
+	}
+
+	free(ptr);
+
+	return (new_ptr);
+}
