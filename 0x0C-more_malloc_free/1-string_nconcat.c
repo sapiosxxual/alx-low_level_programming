2dfddb27adce8e326a52018b43ac35a3c2af6cc5
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-07 23:46:45 -0700

    function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, total_len, i, j;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *newString;
 
-	while (s1[len1] != '\0')
+	while (s1 && s1[len1] != '\0')
 		len1++;
-	if (s2 == NULL)
-		s2 = "";
-	while (s2[len2] != '\0')
+	while (s2 && s2[len2] != '\0')
 		len2++;
-	if (n >= len2)
-		n = len2;
-	total_len = len1 + n + 1;
-
-	newString = malloc(sizeof(char) * total_len);
-
-	if (newString == NULL)
+	if (n < len2)
+		newString = malloc(sizeof(char) * (len1 + n - 1));
+	else
+		newString = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!newString)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 	{
 		newString[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	while (n < len2 && i < (len1 + n))
 	{
-		newString[i + j] = s2[j];
+		newString[i++] = s2[j++];
 	}
-	newString[i + j] = '\0';
-
+	while (n >= len2 && i < (len1 + len2))
+	{
+		newString[i++] = s2[j++];
+	}
+	newString[i] = '\0';
 	return (newString);
 }
