86f5d4b23cc87c8e29bb2b713c0decd2c0b3e156
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-11 22:33:57 -0700

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,20 +17,22 @@ int _strlen(char *s)
 	return (length);
 }
 /**
- * _strcpy - copying a string from source to destination
- * incl terminating null byte
+ * *_strcpy - copying a string from source to destination
+ * incl terminating null byte (0) to the buffer pointed to dest
  * @dest: destination
  * @src: source
  *
  * Return: the copied string
  */
-char *_strcpy(char *src, char *dest)
+char *_strcpy(char *dest, char *src)
 {
-	int i, len;
+	int i;
+	int len = 0;
 
-	i = 0;
 	while (src[len] != '\0')
+	{
 		len++;
+	}
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
