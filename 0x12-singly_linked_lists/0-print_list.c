d13acd19d64abee84ae82f82459f0f69ecf8c936
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-22 15:07:05 -0700

    a function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,7 @@
 /**
  * print_list - function that prints all the elements of a list_t list.
  * @h: pointer to the list_t list to print
+ *
  * Return: the number of nodes to be printed
  */
 size_t print_list(const list_t *h)
