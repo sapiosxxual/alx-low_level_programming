3a0197e8ab0def37e6abd2805a25070cefe08bdf
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-22 18:05:25 -0700

    a function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,15 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		ls++;
 	}
-
 	return (ls);
 }
