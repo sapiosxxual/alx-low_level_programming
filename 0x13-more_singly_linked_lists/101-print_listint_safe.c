0837733b399284d7a84841ce054df8c04ca0e400
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-27 14:58:37 -0700

    function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -22,7 +22,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (temp <= current)
 		{
 			printf("-> [%p] %d\n", (void *)current, current->n);
-			exit (98);
+			exit(98);
 		}
 	}
 	return (count);
