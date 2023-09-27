690c0c75a10cd598dbd3c957b60fa22ea0d1b283
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-27 14:57:02 -0700

    function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,22 +10,20 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current = head;
+	const listint_t *temp = NULL;
 
 	while (current != NULL)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
 
-		if (current->next >= current)
-		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
-		}
+		temp = current;
 		current = current->next;
-	}
-	if (ferror(stdout))
-	{
-		exit(98);
+		if (temp <= current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			exit (98);
+		}
 	}
 	return (count);
 }
