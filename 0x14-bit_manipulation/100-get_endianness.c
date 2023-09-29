e5560661b4b1c4b9a6c4d788c6f86dda0496d65e
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-09-28 22:00:54 -0700

    bitwise manipulation

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,7 +3,7 @@
  * get_endianess - checks if its big or small endianess
  * Return: 0 if big 1 for little
  */
-int get_endianess(void)
+int get_endianness(void)
 {
 	unsigned int i = 1;
 	char *c = (char *) &i;
