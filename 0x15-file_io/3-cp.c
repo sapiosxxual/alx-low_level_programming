1173aee47b5010be40a451aeba27334a43a0646c
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-10-03 18:13:41 -0700

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 		close_file(file_from);
 		exit(99);
 	}
