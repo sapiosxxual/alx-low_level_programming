30f98f7f2fb1f24b5f896098fd365ea7cc852afb
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-10-04 01:52:12 -0700

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,52 +46,36 @@ void close_file(int file_d)
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, r, w;
-	char buffer[1024];
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = create_buff(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	r = read(file_from, buffer, 1024);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
+	while (r > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close_file(file_from);
-		exit(99);
-	}
-	while ((r = read(file_from, buffer, 1024)) > 0)
-	{
-		if (r == -1)
+		if (file_from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-					argv[1]);
-			close_file(file_from);
-			close_file(file_to);
-			exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
 		}
 		w = write(file_to, buffer, r);
-		if (w == -1 || w != r)
+		if (w == -1 || file_to == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_file(file_from);
-			close_file(file_to);
+			free(buffer);
 			exit(99);
 		}
+		r = read(file_from, buffer, 1024);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
 	}
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
-		close_file(file_from);
-		close_file(file_to);
-		exit(98);
-	}
+	free(buffer);
 	close_file(file_from);
 	close_file(file_to);
 	return (0);
