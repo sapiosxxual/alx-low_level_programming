57901b5b1c637e4a4b62a4cf1a3dc201eb4c892b
Okonji Victor Oluwadamilare <okonjivictor49@gmail.com>
2023-10-03 18:55:19 -0700

    C - File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,6 +47,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, r, w;
 	char buffer[1024];
+	ssize_t total_written = 0;
 
 	if (argc != 3)
 	{
@@ -76,13 +77,17 @@ int main(int argc, char *argv[])
 			close_file(file_to);
 			exit(98);
 		}
-		w = write(file_to, buffer, r);
-		if (w == -1)
+		while (total_written < r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_file(file_from);
-			close_file(file_to);
-			exit(99);
+			w = write(file_to, buffer + total_written, r - total_written);
+			if (w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				close_file(file_from);
+				close_file(file_to);
+				exit(99);
+			}
+			total_written += w;
 		}
 	}
 	if (r == -1)
